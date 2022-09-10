f2a9031917ba6783c6adc4f4aef0571c9b56100d
Hector Garza <hggumich@gmail.com>
2022-09-10 10:44:01 -0400

    add rand

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
+use std::io;
+use rand::Rng;
+
 fn main() {
     println!("Guess the number!");
 
-    print!("Please enter your guess.")
+    let secret_number = rand::thread_rng().gen_range(1);
+
+    println!("The secret number is: {}", secret_number);
+
+    print!("Please enter your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
 }
